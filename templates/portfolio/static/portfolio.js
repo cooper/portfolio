ffcad3c7395b881528d8f118d9d7afd87a473799
Mitchell Cooper <githubspam@rlygd.net>
2020-03-13 15:55:47 -0400

    set background-image so as to not override background-size and background-position

--- templates/portfolio/static/portfolio.js
+++ templates/portfolio/static/portfolio.js
@@ -2,7 +2,7 @@ document.addEvent('domready', function () {
     $$('img.portfolio-gallery-preview').each(function (img) {
         var setBg = img.getParent('.main-wrapper');
         if (setBg) {
-            setBg.setStyle('background', "url('" + img.src + "')")
+            setBg.setStyle('background-image', "url('" + img.src + "')")
             setBg.addClass('image-background');
         }
         img.destroy();
